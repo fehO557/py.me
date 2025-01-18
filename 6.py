36c2769a1f830942fb752efa1660977a20094148
feh <127210537+fehO557@users.noreply.github.com>
2025-01-17 23:51:16 -0300

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,8 @@
+#Exercise 2 shopping cart program
+
+item = input("what item would you like to buy?: ")
+price = float(input("what is the price?: "))
+quantity = int(input("How many wolud you like: "))
+total = price * quantity
+print(f"you have bought {quantity} x {item}/s")
+print(f"your total is: {total}")
