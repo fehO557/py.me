6222da532e34bf1f3e3da57ba3867565d704bff5
feh <127210537+fehO557@users.noreply.github.com>
2025-01-19 00:51:38 -0300

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,16 @@
+
+friends = 10
+#friends = friends + 1
+#friends += 1
+#friends = friends - 2
+#friends -= 2
+#friends = friends * 3
+#friends *= 3
+#friends = friends / 2
+#friends /= 2
+#friends = friends **2
+#friends **= 2
+remainder = friends % 2
+
+print(f"you have {friends} friend/s")
+print(remainder)
