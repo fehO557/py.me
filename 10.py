0846ed3d383c16062aa3da4a3952b5df39cb39bc
feh <127210537+fehO557@users.noreply.github.com>
2025-01-19 01:07:25 -0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,14 @@
+import math
+#para utilizar o valor de pi
+#print(math.pi)
+#para euler utilizamos
+#print(math.e)
+x = 9.9
+#result = math.sqrt(x)
+#como pode ver math.sqrt() é a raiz quadrada
+#result = math.ceil(x)
+#math.ceil() é utilizado para arredondar para cima
+result = math.floor(x)
+#math.floor() é usado para arredondar para baixo
+
+print(result)
