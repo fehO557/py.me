86364c3c4e819ef52145c0b9df82d151532745f3
feh <127210537+fehO557@users.noreply.github.com>
2025-01-19 00:52:09 -0300

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,13 @@
+x = 3.14
+y = 4
+z = 5
+
+#round is used to  round a value
+#result = round(x)
+#distance a number from 0
+#result = abs(y)
+#pow(4,3) is the same that 4**3
+#result = pow(4,3)
+
+result = max(x, y, z)
+print(result)
